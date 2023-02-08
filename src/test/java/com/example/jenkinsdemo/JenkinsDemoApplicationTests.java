99829c360342e5e0886141883f813b192e3394b5
Matvey Lazarev <matv26-01@mail.ru>
2023-02-08 18:09:27 +0300

    Initial commit

---
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.jenkinsdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        log.info("Tests are running...");
+        Assertions.assertTrue(true);
+    }
+}
